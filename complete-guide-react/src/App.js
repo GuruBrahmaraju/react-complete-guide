61ca5e155f6ecea5e2de89760cff60e21f801b24
Guru <Guru@AGANITH>
2018-11-30 10:46:32 +0530

    Updateing the static content and  JSX

--- complete-guide-react/src/App.js
+++ complete-guide-react/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>Hi, I am React App</h1>
+     <div className="App">
+      <h1>Hi, I am React App</h1>
+      <p>This is realy Working on the Paragraph</p>
+      <Person />
       </div>
-    );
+     );
+    //Understanding the JSX
+   // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Does this Work Now?'))
+  // JSX Restraction
+  //class, in jsx className in view it takes class not class name 
+  // jsx need root extensation element 
   }
 }
 
-export default App;
+export default App; 
