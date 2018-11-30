dc208245a38b0bfed54e5ebedeac0530548fc397
Guru <Guru@AGANITH>
2018-11-30 16:11:52 +0530

    Done With Props and State and Bind

--- complete-guide-react/src/App.js
+++ complete-guide-react/src/App.js
@@ -3,14 +3,47 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    //javascript Object {}
+    persons : [
+      {name : 'Max', age: 32},
+      {name : 'Manu', age: 29},
+      {name : 'Stephanie', age: 26}
+    ],
+    otherState : 'Some Other Value' 
+  }
+
+  switchNameHandler = (newName) => {
+    //console.log("Welcome to Clicking the Button")
+  // DONT DO THIS :this.state.persons[0].name = 'GuruBrahmaraju';
+    this.setState({
+      persons : [
+        {name : newName, age: 31},
+        {name : 'Manu', age: 29},
+        {name : 'Stephanie', age: 27}
+      ]
+    })
+}
   render() {
     return (
      <div className="App">
       <h1>Hi, I am React App</h1>
       <p>This is realy Working on the Paragraph</p>
-      <Person name = 'Guru' age = '29'/>
-      <Person name = 'Naveen' age = '28'>My Hobbies : Racing </Person>
-      <Person name = 'Shiva' age = '26'/>
+      
+      <button onClick = {this.switchNameHandler.bind(this, 'Maximilian')}>Switch Names</button><p></p>
+      <button onClick = {() => this.switchNameHandler('Maximilian !!!')} >Switch ReturnFunction Name </button>
+      <Person 
+       name = {this.state.persons[0].name} 
+       age = {this.state.persons[0].age} />
+      <Person 
+        name = {this.state.persons[1].name}
+         age = {this.state.persons[1].age}
+         click = {this.switchNameHandler.bind(this, 'Max!')}>
+         My Hobbies : Racing</Person>
+      <Person 
+       name = {this.state.persons[2].name} 
+        age = {this.state.persons[2].age} />
+
       </div>
      );
     //Understanding the JSX
--- complete-guide-react/src/Person/Person.js
+++ complete-guide-react/src/Person/Person.js
@@ -3,10 +3,12 @@ import '../index.css';
 const person = (props) => {
 
  return(<div>
+     
+     {/* we cant set at this place state */}
      {/* <p>I'm a Person ! {Math.floor(Math.random() * 30)} Years Old</p>
      <p>Guru is {Math.floor(Math.random() * 30)} Years Old</p>*/}
      <div className="person">
-     <p >I am {props.name} and {props.age} Years Old</p> 
+     <p onClick = {props.click}>I am {props.name} and {props.age} Years Old</p> 
      <p>{props.children}</p>
      </div>
  </div> 
